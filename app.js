435f80cde6c040aff35bfc6d624e34736ebccab4
guilhermesouza11 <guilhermesouzadossantos11@gmail.com>
2021-09-06 17:44:46 -0300

    Conclusão do arquivo JS

---
+++ app.js
@@ -0,0 +1,31 @@
+
+const calcular = document.getElementById('calcular');
+
+
+function imc (){
+  const nome = document.getElementById('nome').value;
+  const altura = document.getElementById('altura').value;
+  const peso = document.getElementById('peso').value;
+  const resultado = document.getElementById('resultado');
+
+  if (nome !== '' && altura !== '' && peso !== ''){
+    const valorIMC = (peso / (altura * altura)).toFixed(2);
+    
+    let classificacao = '';
+    
+    if(valorIMC < 18.5){
+      classificacao = 'abaixo do peso'
+    }else if(valorIMC > 18.5 && valorIMC < 24.9){
+      classificacao = 'com o peso na média'
+    }else{
+      classificacao = 'acima do peso'
+    };
+
+    resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}.`;
+
+  }else{
+    resultado.textContent = 'Preencha todos os campos!!!';
+  } 
+}
+
+calcular.addEventListener('click', imc);
